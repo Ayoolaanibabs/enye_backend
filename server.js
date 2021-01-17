f23c748992168f7981640737020d9d3a0ced623c
ayo <hakeemanibaba@yahoo.com>
2021-01-17 16:10:12 +0100

    changes

--- server.js
+++ server.js
@@ -10,18 +10,20 @@ app.get('/api/rates', (req,res,next)=>{
             return response.json();
         })
         .then(data=>{
-            res.send({
+            var output = { 
                 results:{
-                    base: data.base,
-                    date: data.date,
-                    rates: data.rates
-                }
-            });
+                base: data.base,
+                date: data.date,
+                rates: data.rates
+            }
+        }
+        res.send(output);
         })
         .catch(error=>{
             res.status(404)
             res.send(error)
         })
+        
 })  
 app.use((req, res, next)=>{
     res.status(404);
