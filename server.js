a30bf91b56d3de70f02499e1c6822643c2e43f30
ayo <hakeemanibaba@yahoo.com>
2021-01-21 11:19:29 +0100

    Fixes to error handling

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.get('/api/rates', (req,res,next)=>{
-        let base = req.query.base;
-        let currency = req.query.currency;
+        const base = req.query.base;
+        const currency = req.query.currency;
         fetch(`https://api.exchangeratesapi.io/latest?base=${base}&symbols=${currency}`)
         .then(response=>{
-            return response.json();
-        })
-        .then(data=>{
-            var output = { 
-                results:{
-                base: data.base,
-                date: data.date,
-                rates: data.rates
+            if(response.ok){
+            response.json().then(data=>{
+                var output = { 
+                    results:{
+                    base: data.base,
+                    date: data.date,
+                    rates: data.rates
+                }
             }
-        }
-        res.send(output);
+            res.send(output);
+            })
+            }else{
+                throw 'Something went wrong'
+            }
         })
         .catch(error=>{
             res.json({
@@ -26,14 +29,7 @@ app.get('/api/rates', (req,res,next)=>{
             })
         })
         
-})
-
-app.use((req, res, next)=>{
-    res.status(404);
-    res.send({
-        error: 'A problem Occured!'
-    })
-})
+});
 
 PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
